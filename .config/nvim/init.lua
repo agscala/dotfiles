f6ae8bd74665b1879adb16f6826193a10a5dafc8
Andrew Scala <andrew@andrews-mbp.lan>
2021-05-16 21:56:09 -0400

    nvim setup

---
+++ .config/nvim/init.lua
@@ -0,0 +1,119 @@
+local execute = vim.api.nvim_command
+local fn = vim.fn
+
+local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+
+if fn.empty(fn.glob(install_path)) > 0 then
+  fn.system({'git', 'clone', 'https://github.com/wbthomason/packer.nvim', install_path})
+  execute 'packadd packer.nvim'
+end
+
+local packer = require("packer")
+local use = packer.use
+
+-- using { } for using different branch , loading plugin with certain commands etc
+require("packer").startup(
+    function()
+        use "wbthomason/packer.nvim"
+
+        -- color related stuff
+        use "siduck76/nvim-base16.lua"
+        use "norcalli/nvim-colorizer.lua"
+
+        -- lsp stuff
+        use "nvim-treesitter/nvim-treesitter"
+        use "neovim/nvim-lspconfig"
+        use "hrsh7th/nvim-compe"
+        use "onsails/lspkind-nvim"
+        use "sbdchd/neoformat"
+        use "nvim-lua/plenary.nvim"
+
+        use "mhinz/vim-signify"
+        use "akinsho/nvim-bufferline.lua"
+        use "glepnir/galaxyline.nvim"
+        use "windwp/nvim-autopairs"
+        use "alvan/vim-closetag"
+
+        -- file managing , picker etc
+        use "kyazdani42/nvim-tree.lua"
+        use "kyazdani42/nvim-web-devicons"
+        use "ryanoasis/vim-devicons"
+        use "nvim-telescope/telescope.nvim"
+        use "nvim-telescope/telescope-media-files.nvim"
+        use "nvim-lua/popup.nvim"
+
+        -- misc
+        use "tweekmonster/startuptime.vim"
+        use "907th/vim-auto-save"
+        use "kdav5758/TrueZen.nvim"
+        use "folke/which-key.nvim"
+
+        -- discord rich presence
+        --use "andweeb/presence.nvim"
+
+        use {"lukas-reineke/indent-blankline.nvim", branch = "lua"}
+    end,
+    {
+        display = {
+            border = { "┌", "─", "┐", "│", "┘", "─", "└", "│" }
+        }
+    }
+)
+
+-- load all plugins
+require "file-icons"
+
+require "misc-utils"
+require "top-bufferline"
+require "statusline"
+
+require("colorizer").setup()
+
+-- lsp stuff
+require "nvim-lspconfig"
+require "compe-completion"
+
+local cmd = vim.cmd
+local g = vim.g
+
+g.mapleader = ","
+g.auto_save = 0
+
+-- colorscheme related stuff
+cmd "syntax on"
+
+local base16 = require "base16"
+base16(base16.themes["onedark"], true)
+
+require "custom_highlights"
+
+-- blankline
+
+local indent = 2
+
+g.indentLine_enabled = 1
+g.indent_blankline_char = "▏"
+
+g.indent_blankline_filetype_exclude = {"help", "terminal"}
+g.indent_blankline_buftype_exclude = {"terminal"}
+
+g.indent_blankline_show_trailing_blankline_indent = false
+g.indent_blankline_show_first_indent_level = false
+
+require "treesitter-nvim"
+require "mappings"
+
+require "telescope-nvim"
+require "nvimTree"
+
+-- git signs , lsp symbols etc
+require("nvim-autopairs").setup()
+require("lspkind").init()
+
+-- hide line numbers in terminal windows
+vim.api.nvim_exec([[
+   au BufEnter term://* setlocal nonumber
+]], false)
+
+-- setup for TrueZen.nvim
+require "zenmode"
---
+++ .config/nvim/lua/mappings.lua
@@ -0,0 +1,21 @@
+local function map(mode, lhs, rhs, opts)
+    local options = {noremap = true}
+    if opts then
+        options = vim.tbl_extend("force", options, opts)
+    end
+    vim.api.nvim_set_keymap(mode, lhs, rhs, options)
+end
+
+local opt = {}
+
+-- dont copy any deleted text , this is disabled by default so uncomment the below mappings if you want them!
+--[[ remove this line
+
+map("n", "dd", [=[ "_dd ]=], opt)
+map("v", "dd", [=[ "_dd ]=], opt)
+map("v", "x", [=[ "_x ]=], opt)
+
+ this line too ]]
+
+-- OPEN TERMINALS --
+-- map("n", "<C-l>", [[<Cmd>vnew term://bash <CR>]], opt) -- over right
