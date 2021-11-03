66078a28817360da5384f02602dceab6f80101ad
Andrew Scala <agscala@gmail.com>
2021-11-03 11:03:15 -0400

    nvimtree config, replacing lightspeed with hop

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -77,7 +77,8 @@ require("packer").startup(
         use "907th/vim-auto-save"
         use "folke/which-key.nvim"
 
-        use 'ggandor/lightspeed.nvim' -- quick movement
+        -- use 'ggandor/lightspeed.nvim' -- quick movement
+        use {'phaazon/hop.nvim', branch = 'v1'}
 
 
         -- discord rich presence
@@ -191,23 +192,29 @@ catppuccino.load()
 
 require "custom_highlights"
 
+-- hop
+vim.api.nvim_set_keymap('n', 'f', "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.AFTER_CURSOR, current_line_only = true })<cr>", {})
+vim.api.nvim_set_keymap('n', 'F', "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.BEFORE_CURSOR, current_line_only = true })<cr>", {})
+require'hop'.setup { keys = 'etovxqpdygfblzhckisuran' }
+
+
 -- lightspeed
-require'lightspeed'.setup {
-  jump_to_first_match = true,
-  jump_on_partial_input_safety_timeout = 400,
-  -- This can get _really_ slow if the window has a lot of content,
-  -- turn it on only if your machine can always cope with it.
-  highlight_unique_chars = false,
-  grey_out_search_area = true,
-  match_only_the_start_of_same_char_seqs = true,
-  limit_ft_matches = 5,
-  full_inclusive_prefix_key = '<c-x>',
-  -- By default, the values of these will be decided at runtime,
-  -- based on `jump_to_first_match`.
-  labels = nil,
-  -- cycle_group_fwd_key = nil,
-  -- cycle_group_bwd_key = nil,
-}
+-- require'lightspeed'.setup {
+  -- jump_to_first_match = true,
+  -- jump_on_partial_input_safety_timeout = 400,
+  -- -- This can get _really_ slow if the window has a lot of content,
+  -- -- turn it on only if your machine can always cope with it.
+  -- highlight_unique_chars = false,
+  -- grey_out_search_area = true,
+  -- match_only_the_start_of_same_char_seqs = true,
+  -- limit_ft_matches = 5,
+  -- full_inclusive_prefix_key = '<c-x>',
+  -- -- By default, the values of these will be decided at runtime,
+  -- -- based on `jump_to_first_match`.
+  -- labels = nil,
+  -- -- cycle_group_fwd_key = nil,
+  -- -- cycle_group_bwd_key = nil,
+-- }
 
 -- blankline
 
--- .config/nvim/lua/nvimTree.lua
+++ .config/nvim/lua/nvimTree.lua
@@ -4,16 +4,15 @@ vim.o.termguicolors = true
 
 g.nvim_tree_side = "left"
 g.nvim_tree_width = 50
-g.nvim_tree_ignore = {".git", "node_modules", ".cache"}
-g.nvim_tree_auto_open = 0
-g.nvim_tree_auto_close = 0
+-- g.nvim_tree_auto_open = 0
+-- g.nvim_tree_auto_close = 0
 g.nvim_tree_quit_on_open = 0
-g.nvim_tree_follow = 1
+-- g.nvim_tree_follow = 1
 g.nvim_tree_indent_markers = 1
-g.nvim_tree_hide_dotfiles = 1
+-- g.nvim_tree_hide_dotfiles = 1
 g.nvim_tree_git_hl = 1
 g.nvim_tree_root_folder_modifier = ":t"
-g.nvim_tree_tab_open = 0
+-- g.nvim_tree_tab_open = 0
 g.nvim_tree_allow_resize = 1
 
 g.nvim_tree_show_icons = {
@@ -88,6 +87,7 @@ require'nvim-tree'.setup {
   open_on_tab         = false,
   hijack_cursor       = false,
   update_cwd          = false,
+  nvim_tree_ignore    = {".git", "node_modules", ".cache"},
   update_to_buf_dir   = {
     enable = true,
     auto_open = true,
