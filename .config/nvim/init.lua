730b1b84cc8a367c66070dcd7b680d1511bb12cc
Andrew Scala <agscala@gmail.com>
2021-12-15 17:29:19 -0500

    treesitter improvements

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -231,7 +231,7 @@ g.indent_blankline_buftype_exclude = {"terminal"}
 g.indent_blankline_show_trailing_blankline_indent = true
 g.indent_blankline_show_first_indent_level = true
 
-require "treesitter-nvim"
+require "config-treesitter"
 --require "mappings"
 
 require "config-telescope"
--- .config/nvim/lua/config-treesitter.lua
+++ .config/nvim/lua/config-treesitter.lua
@@ -1,8 +1,23 @@
 local ts_config = require("nvim-treesitter.configs")
 
 ts_config.setup {
+    -- ensure_installed = "all",
     ensure_installed = {
         "javascript",
+        "typescript",
+        "markdown",
+        "elixir",
+        "c_sharp",
+        "c",
+        "rust",
+        "go",
+        "graphql",
+        "ruby",
+        "rust",
+        "svelte",
+        "tsx",
+        "vue",
+        "yaml",
         "html",
         "css",
         "bash",
