116561d7ce71fec5504e4abfc554b8adce74f374
Andrew Scala <agscala@gmail.com>
2023-11-17 11:40:38 -0500

    fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -8,6 +8,7 @@ source ~/.config/fish/api-keys.fish
 set PATH ~/.node_modules/bin $PATH
 set PATH ~/bin $PATH
 set PATH /Users/andrew/.meteor $PATH
+set PATH /Users/andrew/.local/bin $PATH
 
 set -x LDFLAGS "-L/usr/local/opt/zlib/lib"
 set -x CPPFLAGS "-I/usr/local/opt/zlib/include"
@@ -15,3 +16,10 @@ set -x CPPFLAGS "-I/usr/local/opt/zlib/include"
 # load_nvm
 
 fish_add_path /usr/local/opt/sphinx-doc/bin
+
+# pnpm
+set -gx PNPM_HOME "/Users/andrew/Library/pnpm"
+if not string match -q -- $PNPM_HOME $PATH
+  set -gx PATH "$PNPM_HOME" $PATH
+end
+# pnpm end
