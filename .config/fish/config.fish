c7622619d01a7aac17606c9584730c48abc4ce74
Andrew Scala <andrew.scala@zapier.com>
2024-02-13 11:39:51 -0500

    Fixing path issues

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,12 +2,14 @@ alias vim 'nvim'
 alias vi 'nvim'
 alias vault 'aws-vault exec zapier --'
 
+set -Ux EDITOR vim
+
 source ~/.config/fish/api-keys.fish
 
 set PATH ~/.node_modules/bin $PATH
 set PATH ~/bin $PATH
-set PATH /Users/andrew/.meteor $PATH
-set PATH /Users/andrew/.local/bin $PATH
+set PATH /Users/agscala/.meteor $PATH
+set PATH /Users/agscala/.local/bin $PATH
 
 set -x LDFLAGS "-L/usr/local/opt/zlib/lib"
 set -x CPPFLAGS "-I/usr/local/opt/zlib/include"
@@ -17,7 +19,7 @@ set -x CPPFLAGS "-I/usr/local/opt/zlib/include"
 fish_add_path /usr/local/opt/sphinx-doc/bin
 
 # pnpm
-set -gx PNPM_HOME "/Users/andrew/Library/pnpm"
+set -gx PNPM_HOME "/Users/agscala/Library/pnpm"
 if not string match -q -- $PNPM_HOME $PATH
   set -gx PATH "$PNPM_HOME" $PATH
 end
